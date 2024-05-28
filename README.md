d1b58b83e0125aaf629668db661ec7ec28c724cb
mcei <mcwave@ya.ru>
2024-05-28 22:41:30 +0300

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+Приложение BookInfo для хранения и обработки информации о книгах
+
+Приложение позволяет:
+- добавить информацию о книге в хранилище
+- обновить информацию
+- удалить информацию
+- запросить информацию о книге
+- запросить информацию о всех книгах
+
+Использование:
+
+```
+curl -X GET localhost:8000/books
+
+curl -X GET localhost:8000/books/1
+
+curl -X POST localhost:8000/books --data '{"ID":1,"Title":"Mybook1","Author":"Author1","Year":"2021"}'
+
+curl -X PUT localhost:8000/books --data '{"ID":1,"Title":"Mybook1","Author":"Author2","Year":"2022"}'
+
+curl -X DELETE localhost:8000/books/1
+```
+
+
+Notes:
+ Standard Go Layout: https://github.com/golang-standards/project-layout/
---
+++ storage/db/mongo.go
@@ -0,0 +1,71 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"bookinfo/models"
+)
+
+type BookDB struct{}
+
+func (b BookDB) GetBooks(collection *mongo.Collection, book models.Book, books []models.Book) []models.Book {
+	ctx := context.Background()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cursor.All(ctx, &books)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(ctx)
+
+	return books
+}
+
+func (b BookDB) GetBook(collection *mongo.Collection, book models.Book, id int) models.Book {
+	filter := bson.D{{"id", id}}
+
+	err := collection.FindOne(context.Background(), filter).Decode(&book)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// failed if id does not exist
+	return book
+}
+
+func (b BookDB) AddBook(collection *mongo.Collection, book models.Book) string {
+	insertResult, err := collection.InsertOne(context.Background(), book)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%v", insertResult.InsertedID)
+}
+
+func (b BookDB) UpdateBook(collection *mongo.Collection, book models.Book) string {
+	filter := bson.D{{"id", book.ID}}
+	update := bson.D{{"$set", bson.D{{"title", book.Title}, {"author", book.Author}, {"year", book.Year}}}}
+
+	updateResult, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%v", updateResult)
+}
+
+func (b BookDB) RemoveBook(collection *mongo.Collection, id int) string {
+	filter := bson.D{{"id", id}}
+	delResult, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%v", delResult)
+}
