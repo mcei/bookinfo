4c3937ee908a10412fd1e039ac0dfbffdc4205e6
mcei <mcwave@ya.ru>
2024-06-12 02:32:00 +0700

    Remove

--- domain/user/usecase.go
+++ domain/user/usecase.go
@@ -1,21 +1,6 @@
 package domain
 
-import (
-	"bookinfo/storage"
-)
-
-//import "bookinfo/storage/db"
-
 type Repository interface {
 	// TODO implement
 	// указывать как зависимость вместо repo db.BookDB
-}
-
-// выкинуть
-//type UseCase struct {
-//	repo db.BookDB
-//}
-
-func NewUseCase(repo storage.BookDB) *UseCase {
-	return &UseCase{repo: repo}
 }
