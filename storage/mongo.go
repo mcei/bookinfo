1e3cbaab2d207a622c476d0318b6eca3a4c222fb
mcei <mcwave@ya.ru>
2024-06-12 02:31:35 +0700

    Refactor mongo.go

--- storage/mongo.go
+++ storage/mongo.go
@@ -3,8 +3,6 @@ package storage
 import (
 	"context"
 	"fmt"
-	"log"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -13,59 +11,52 @@ import (
 
 type BookDB struct{}
 
-func (b BookDB) GetBooks(collection *mongo.Collection, book book.Book, books []book.Book) []book.Book {
-	ctx := context.Background()
-
-	cursor, err := collection.Find(ctx, bson.M{})
+func (b BookDB) GetBooks(ctx context.Context, c *mongo.Collection, books []book.Book) ([]book.Book, error) {
+	cursor, err := c.Find(ctx, bson.M{})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
 	err = cursor.All(ctx, &books)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
 	defer cursor.Close(ctx)
-
-	return books
+	return books, nil
 }
 
-func (b BookDB) GetBook(collection *mongo.Collection, book book.Book, id int) book.Book {
+func (b BookDB) GetBook(ctx context.Context, c *mongo.Collection, book book.Book, id int) (book.Book, error) {
 	filter := bson.D{{"id", id}}
-
-	err := collection.FindOne(context.Background(), filter).Decode(&book)
+	err := c.FindOne(ctx, filter).Decode(&book)
 	if err != nil {
-		log.Fatal(err)
+		return book, err
 	}
 	// failed if id does not exist
-	return book
+	return book, nil
 }
 
-func (b BookDB) AddBook(collection *mongo.Collection, book book.Book) string {
-	insertResult, err := collection.InsertOne(context.Background(), book)
+func (b BookDB) AddBook(ctx context.Context, c *mongo.Collection, book book.Book) (string, error) {
+	insertResult, err := c.InsertOne(ctx, book)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	return fmt.Sprintf("%v", insertResult.InsertedID)
+	return fmt.Sprintf("%v", insertResult.InsertedID), nil
 }
 
-func (b BookDB) UpdateBook(collection *mongo.Collection, book book.Book) string {
+func (b BookDB) UpdateBook(ctx context.Context, c *mongo.Collection, book book.Book) (string, error) {
 	filter := bson.D{{"id", book.ID}}
 	update := bson.D{{"$set", bson.D{{"title", book.Title}, {"author", book.Author}, {"year", book.Year}}}}
-
-	updateResult, err := collection.UpdateOne(context.Background(), filter, update)
+	updateResult, err := c.UpdateOne(ctx, filter, update)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	return fmt.Sprintf("%v", updateResult)
+	return fmt.Sprintf("%v", updateResult), nil
 }
 
-func (b BookDB) RemoveBook(collection *mongo.Collection, id int) string {
+func (b BookDB) RemoveBook(ctx context.Context, c *mongo.Collection, id int) (string, error) {
 	filter := bson.D{{"id", id}}
-	delResult, err := collection.DeleteOne(context.Background(), filter)
+	delResult, err := c.DeleteOne(ctx, filter)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	return fmt.Sprintf("%v", delResult)
+	return fmt.Sprintf("%v", delResult), nil
 }
